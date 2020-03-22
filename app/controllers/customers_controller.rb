34f33b82fb9c47af31b45c7a98849bc247bc0275
chirs <chrispod47@gmail.com>
2020-03-22 11:43:20 -0500

    added actions to customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
 class CustomersController < ApplicationController
   def index
+    @customer = Customer.all
+  end
+
+  def alphabetical
+    @customers = Customer.order(:full_name)
+  end
+
+  def no_email
+    @customer = Cudtomer.where(email: null)
   end
 end

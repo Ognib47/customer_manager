bb65e1a4e62484cfcd46c36f58328fe02bdad7fd
chirs <chrispod47@gmail.com>
2020-03-22 11:33:25 -0500

    added file picker to active admin using formic

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -2,4 +2,13 @@
 
 ActiveAdmin.register Customer do
   permit_params :full_name, :phone_number, :email, :notes
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
 end
